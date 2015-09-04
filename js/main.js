6c7f0b36d5fed588e2d57cc6955bfcbb07c3a183
c.m.zofei <mihai@netrom.ro>
2015-09-04 08:28:54 +0300

    Start adding react

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ $(function () {
 		$(this).addClass('active');
 	})
 });
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <div className="ui red menu">
+		<a className="header active item">All</a>
+		<a className="item"><i className="user icon"></i>Christina</a>
+		<span className="floated right item">Signed in as <a href="#">Mike</a></span>
+	</div>
+    );
+  }
+});
+
+React.render(
+  <Header />,
+  document.getElementById('content')
+);
