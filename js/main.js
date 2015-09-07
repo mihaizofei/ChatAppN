6dfa78e17ca52be27d9320f76cca1bbe3db4c466
c.m.zofei <mihai@netrom.ro>
2015-09-07 08:32:39 +0300

    More react

--- js/main.js
+++ js/main.js
@@ -5,19 +5,78 @@ $(function () {
 	})
 });
 
+var MainContainer = React.createClass({
+	render: function() {
+		return (
+			<div className='mainContainer'>
+				<Header />
+			</div>
+		);
+	}
+});
+
 var Header = React.createClass({
   render: function() {
     return (
       <div className="ui red menu">
 		<a className="header active item">All</a>
-		<a className="item"><i className="user icon"></i>Christina</a>
+		<a className="item"><i className="user icon"></i>Chris</a>
 		<span className="floated right item">Signed in as <a href="#">Mike</a></span>
 	</div>
     );
   }
 });
 
+var Body = React.createClass({
+	render: function() {
+		return (
+			<div className='bodyContainer'>
+				<Grid />
+			</div>
+		);
+	}
+});
+
+var Grid = React.createClass({
+	render: function() {
+		return (
+			<div className="ui form segment">
+				<div className="ui two column equal height grid">
+					<div className="thirteen wide column">
+						<div className="ui message blue heightleft" style="overflow-y:scroll;">
+							<div className="ui horizontal divider"><i className="wechat icon"></i></div>
+								<div className="ui comments">
+									<Comment />
+								</div>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<a className="avatar">
+					<img src="images/man.png">
+				</a>
+				<div className="content">
+					<a className="author">Michael</a>
+				<div className="metadata">
+					<span className="date">Today at 5:42PM</span>
+				</div>
+				<div className="text">How artistic!</div>
+			</div>
+		);
+	}
+});		
+
 React.render(
-  <Header />,
+  <MainContainer />,
   document.getElementById('content')
 );
