c67b24290bb1f25194375c6ed28d7a1faf9cfc6b
c.m.zofei <mihai@netrom.ro>
2015-09-15 08:33:20 +0300

    more React

--- js/mainReact.js
+++ js/mainReact.js
@@ -2,10 +2,10 @@ var MainContainer = React.createClass({
 	render: function() {
 		return (
 			<div className='mainContainer'>
-				<Header />
+				<Header name="Mike"/>
 				<div className="ui form segment">
 					<Body />
-					<Footer />
+					<Footer name="Mike"/>
 				</div>
 			</div>
 		);
@@ -16,15 +16,13 @@ var Header = React.createClass({
   render: function() {
     return (
       <div className="ui labeled icon menu">
-		<a className="active red item">
-			<i className="users icon"></i>
-			All
-		</a>
-		<a className="red item">
-			<i className="user icon"></i>
-			Chris
-		</a>
-		<span className="floated right item">Signed in as<a href="#">Mike</a></span>
+      	<UserTab tabClass="active red item" iconClass="users icon">
+      		All
+      	</UserTab>
+      	<UserTab tabClass="red item" iconClass="user icon">
+      		Chris
+      	</UserTab>
+		<span className="floated right item">Signed in as<a href="#">{this.props.name}</a></span>
 	</div>
     );
   }
@@ -38,8 +36,12 @@ var Body = React.createClass({
 					<div className="ui message grey heightleft">
 						<div className="ui horizontal divider"><i className="wechat icon"></i></div>
 						<div className="ui comments">
-							<Comment />
-							<Comment />
+							<Comment author="Mike" date="Today at 5:42 PM" image="./images/man.png">
+								How artistic!
+							</Comment>
+							<Comment author="Chris" date="Yesterday at 10:20 AM" image="./images/woman.png">
+								This has been very useful for my research. Thanks as well!
+							</Comment>
 						</div>
 					</div>
 				</div>
@@ -47,8 +49,8 @@ var Body = React.createClass({
 					<div className="ui message grey heightright">
 						<div className="ui horizontal divider"><i className="users icon"></i></div>
 						<div className="ui list">
-							<User />
-							<User />
+							<User name="Chris" image="./images/woman.png"/>
+							<User name="Mike" image="./images/man.png"/>
 						</div>
 					</div>
 				</div>
@@ -57,13 +59,24 @@ var Body = React.createClass({
 	}
 });
 
+var UserTab = React.createClass({
+	render: function() {
+		return (
+			<a className={this.props.tabClass}>
+				<i className={this.props.iconClass}></i>
+				{this.props.children}
+			</a>
+		);
+	}
+});
+
 var User = React.createClass({
 	render: function() {
 		return (
 			<div className="item">
-				<a className="ui mini image"><img src="./images/woman.png"></img></a>
+				<a className="ui mini image"><img src={this.props.image}></img></a>
 				<div className="middle aligned content">
-					<div className="header">Chris</div>
+					<div className="header">{this.props.name}</div>
 				</div>
 			</div>
 		);
@@ -75,14 +88,14 @@ var Comment = React.createClass({
 		return (
 			<div className="comment">
 				<a className="avatar">
-					<img src="./images/man.png"></img>
+					<img src={this.props.image}></img>
 				</a>
 				<div className="content">
-					<a className="author">Michael</a>
+					<a className="author">{this.props.author}</a>
 					<div className="metadata">
-						<span className="date">Today at 5:42PM</span>
+						<span className="date">{this.props.date}</span>
 					</div>
-					<div className="text">How artistic!</div>
+					<div className="text">{this.props.children}</div>
 				</div>
 			</div>	
 		);
@@ -94,7 +107,7 @@ var Footer = React.createClass({
 		return (
 			<div className="ui labeled input">
 				<div className="ui black label">Mike</div>
-				<input placeholder="Enter your text here" name="userText" type="text"></input>
+				<input placeholder="Enter your text here" name={this.props.name} type="text"></input>
 				<SendButton />
 			</div>
 		);
@@ -116,3 +129,8 @@ React.render(
   <MainContainer />,
   document.getElementById('content')
 );
+
+var data = [
+  {author: "Mike", text: "This is one comment"},
+  {author: "Chris", text: "This is *another* comment"}
+];
