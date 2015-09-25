d572de974f9d93a6e72d4b6ad77c91a3e2440531
c.m.zofei <mihai@netrom.ro>
2015-09-25 11:20:00 +0300

    Add get current date to message

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -9,7 +9,7 @@ var Footer = React.createClass({
 		if (!text) {
       		return;
     	}
-    	this.props.onCommentSubmit({author: "Mike", text: text, date: "Today at 5:42 PM", image: "./images/man.png"});
+    	this.props.onCommentSubmit({author: this.props.name, text: text, date: GetDate(), image: "./images/man.png"});
     	React.findDOMNode(this.refs.text).value = '';
     	AppActions.addComment(text);
 	},
@@ -17,7 +17,7 @@ var Footer = React.createClass({
 		return (
 			<form className="commentForm" onSubmit={this.handleSubmit}>
 				<div className="ui labeled input">
-					<div className="ui black label">Mike</div>
+					<div className="ui black label">{this.props.name}</div>
 					<input placeholder="Enter your text here" name={this.props.name} type="text" ref="text"></input>
 					<SendButton />
 				</div>
@@ -25,5 +25,27 @@ var Footer = React.createClass({
 		);
 	}
 });
+
+function GetDate() {
+	var today = new Date();
+
+	var dd = AddZero(today.getDate());
+	var mm = AddZero(today.getMonth() + 1);
+	var yyyy = today.getFullYear();
+	var hours = AddZero(today.getHours());
+	var minutes = AddZero(today.getMinutes());
+	var seconds = AddZero(today.getSeconds());
+
+
+	today = hours + ':' + minutes + ':' + seconds + ' - ' + dd + '/'+ mm + '/' + yyyy;
+	return today;
+};
+
+function AddZero(val) {
+	if (val < 10) {
+		val = '0' + val;
+	}
+	return val;
+}
 
 module.exports = Footer;
